1b4dca75d75c6db45489554396a86a9dd9d106e6
Jan Allersma <janallersma@gmail.com>
2018-07-13 13:35:06 +0200

    Implement 'alsa library is not found' panic.

--- build.rs
+++ build.rs
@@ -1,5 +1,25 @@
 extern crate pkg_config;
 
 fn main() {
-    pkg_config::find_library("alsa").unwrap();
+    let missing: String = String::from("\"pkg-config\" \"--libs\" \"--cflags\" \"alsa\"");
+
+    match pkg_config::probe_library("alsa") {
+        Ok(_) => {},
+        Err(e) => {
+            match e {
+                pkg_config::Error::Failure {command, output} => {
+                    if command == missing {
+                        panic! (
+                            "Could not find the alsa library. Have you installed the library on your system?\n\n\
+                            For Fedora users:\n# dnf install alsa-lib-devel\n\n\
+                            For Debian/Ubuntu users:\n# apt-get install libasound2\n\n"
+                        );
+                    } else {
+                        panic!("Command '{}' failed. Details:\n{:?}", command, output);
+                    }
+                },
+                _ => panic!("{}", e)
+            }
+        }
+    };
 }
