594e9f763951bc1c532d00c8e8d014ffd1f75b7e
Jan Allersma <janallersma@gmail.com>
2018-07-15 18:18:56 +0200

    Add improvements on 'alsa library not found' panic: * Make code easier to understand by avoiding rightward drift. * Show error details when the library is not found. * Show a different package name in the error message (from 'libasound2' to 'libasound2-dev') when the library is missing.

--- build.rs
+++ build.rs
@@ -1,25 +1,16 @@
 extern crate pkg_config;
 
 fn main() {
-    let missing: String = String::from("\"pkg-config\" \"--libs\" \"--cflags\" \"alsa\"");
-
-    match pkg_config::probe_library("alsa") {
-        Ok(_) => {},
-        Err(e) => {
-            match e {
-                pkg_config::Error::Failure {command, output} => {
-                    if command == missing {
-                        panic! (
-                            "Could not find the alsa library. Have you installed the library on your system?\n\n\
-                            For Fedora users:\n# dnf install alsa-lib-devel\n\n\
-                            For Debian/Ubuntu users:\n# apt-get install libasound2\n\n"
-                        );
-                    } else {
-                        panic!("Command '{}' failed. Details:\n{:?}", command, output);
-                    }
-                },
-                _ => panic!("{}", e)
-            }
+    if let Err(e) = pkg_config::probe_library("alsa") {
+        match e {
+            pkg_config::Error::Failure { .. } => panic! (
+                "Could not find the alsa library. Have you installed the library on your system?\n\n\
+                For Fedora users:\n# dnf install alsa-lib-devel\n\n\
+                For Debian/Ubuntu users:\n# apt-get install libasound2-dev\n\n\
+                pkg_config details:\n{}",
+                e
+            ),
+            _ => panic!("{}", e)
         }
-    };
+    }
 }
